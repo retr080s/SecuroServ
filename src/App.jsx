77ab4184bf4ef743c046bf1874267b0c7063852f
retr080s <retr080s@protonmail.com>
2023-03-27 18:27:19 +0200

    login menu finished (for now)

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,20 @@ function App() {
           />
           <h3 className='login--menu_login'>PLEASE LOG IN</h3>
           <span className='login--menu_span'>
-            Username<input className='login--menu_input' type='text'></input>
+            Username
+            <input
+              className='login--menu_input'
+              type='text'
+              placeholder='Retr080s'
+            ></input>
           </span>
           <span className='login--menu_span'>
             Password
-            <input className='login--menu_input' type='password'></input>
+            <input
+              className='login--menu_input'
+              type='password'
+              placeholder='********'
+            ></input>
           </span>
           <div className='login--menu_bottom'>
             <h4 className='login--menu_bottom-text-version'>VERSION 1.2.5</h4>
