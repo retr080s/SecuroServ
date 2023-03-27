b8d859dd1d849c57871ff5709c8b15a0f50b4a3d
retr080s <retr080s@protonmail.com>
2023-03-27 17:54:21 +0200

    login menu almost done, added input, button etc..

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,25 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <div className='login-menu'>
-        <img src='src/images/securoserv-logo.png' />
-        <h3>PLEASE LOG IN</h3>
-        <div className='input-menu'></div>
+      <div className='login--menu'>
+        <div className='login--menu_info'>
+          <img
+            src='src/images/securoserv-logo.png'
+            className='login--menu_logo'
+          />
+          <h3 className='login--menu_login'>PLEASE LOG IN</h3>
+          <span className='login--menu_span'>
+            Username<input className='login--menu_input' type='text'></input>
+          </span>
+          <span className='login--menu_span'>
+            Password
+            <input className='login--menu_input' type='password'></input>
+          </span>
+          <div className='login--menu_bottom'>
+            <h4 className='login--menu_bottom-text-version'>VERSION 1.2.5</h4>
+            <button className='login--menu_bottom-button'>Log in</button>
+          </div>
+        </div>
       </div>
     </div>
   );
